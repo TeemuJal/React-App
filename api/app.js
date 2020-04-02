62411bba1df74b11e9819ea9feb457f47a3427e2
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-04-02 12:47:18 +0300

    Added cors and cookie-parser to Node

--- api/app.js
+++ api/app.js
@@ -1,10 +1,18 @@
 var express = require("express");
+var cors = require("cors");
+var cookieParser = require('cookie-parser');
+
 var app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res, next) => {
     res.send("Hello world!");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(9000, () => {
+    console.log("Server running on port 9000");
 });
