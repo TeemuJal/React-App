ab6e389622607d2627c5db9317d7622e67ae57dc
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-04-19 12:34:03 +0300

    Added server-side checking of email and password validity when registering

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var cors = require("cors");
 var cookieParser = require('cookie-parser');
+var emailValidator = require("email-validator");
+var passwordValidator = require('password-validator');
 
 var app = express();
 
@@ -9,8 +11,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Password validator schema
+var schema = new passwordValidator();
+
+schema
+.is().min(8)
+.is().max(72)
+.has().digits()
+.has().not().spaces();
+
 app.get("/", (req, res, next) => {
     res.send("Hello world!");
+});
+
+app.post("/user/register", (req, res, next) => {
+    console.log(req.body);
+
+    const email = req.body.email;
+    const plaintextPassword = req.body.password;
+
+    // Validate email and password on server-side as well
+    if (emailValidator.validate(email) && schema.validate(plaintextPassword)) {
+        console.log("Valid email and password.");
+
+        // TODO hash and salt password using Bcrypt
+        // TODO create user in DB
+
+        res.type("json");
+        res.json("Registered successfully!");
+    }
+    else {
+        console.log("Invalid email/password.");
+
+        res.status(400);
+        res.json("Invalid email/password.")
+    }
 });
 
 app.listen(9000, () => {
