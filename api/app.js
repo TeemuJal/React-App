121bfd00b7b8e178900a9b341bc0b763166798a8
Teemu Jalonen <teemu.jalonen@student.tut.fi>
2020-04-19 13:25:17 +0300

    User is saved to the db upon successful registration

--- api/app.js
+++ api/app.js
@@ -3,6 +3,9 @@ var cors = require("cors");
 var cookieParser = require('cookie-parser');
 var emailValidator = require("email-validator");
 var passwordValidator = require('password-validator');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+const mongoose = require('mongoose');
 
 var app = express();
 
@@ -10,6 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+mongoose.connect('mongodb://localhost/mydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const User = require("./src/User.model")
 
 // Password validator schema
 var schema = new passwordValidator();
@@ -27,16 +37,29 @@ app.get("/", (req, res, next) => {
 app.post("/user/register", (req, res, next) => {
     console.log(req.body);
 
-    const email = req.body.email;
+    const userEmail = req.body.email;
     const plaintextPassword = req.body.password;
 
     // Validate email and password on server-side as well
-    if (emailValidator.validate(email) && schema.validate(plaintextPassword)) {
+    if (emailValidator.validate(userEmail) && schema.validate(plaintextPassword)) {
         console.log("Valid email and password.");
 
-        // TODO hash and salt password using Bcrypt
-        // TODO create user in DB
+        // TODO check if user with the email exists
 
+        bcrypt.hash(plaintextPassword, saltRounds, async function(err, hash) {
+            console.log("password hashed: " + hash);
+
+            const user = new User({ email: userEmail, password: hash });
+
+            await user.save().then(function () {
+                console.log("user created");
+            })
+            .catch(function (err) {
+                console.log("Registration failed.")
+                console.log(err)
+                res.status(400).send("Registration failed.");
+            });
+        });
         res.type("json");
         res.json("Registered successfully!");
     }
