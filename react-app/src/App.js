8560e4851090f302b1d7d79fd5d917877754fa3e
Teemu Jalonen <teemu.jalonen@student.tut.fi>
2020-04-23 17:34:39 +0300

    Added Login functionality with basic routing and authentication

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,74 @@
 import React from 'react';
 import './App.css';
+import { Route, Redirect } from 'react-router-dom';
+import Login from './loginForm';
 import RegistrationForm from './registrationForm';
+import Posts from './postsPage';
 
-function App() {
+const initialState = {
+  loggedIn: null
+};
+
+export default class App extends React.Component {
+  state = { 
+    redirect: false,
+    loggedIn: initialState.loggedIn,
+    token: JSON.parse(localStorage.getItem("userToken"))
+  };
+  
+  handler = (val) => {
+    console.log(val);
+    this.setState({
+      loggedIn: val
+    })
+  }
+  componentDidMount() {
+    const token = this.state.token;
+    // console.log(token);
+    const searchParams = new URLSearchParams({"secret_token": token})
+    // Check whether user is logged in TODO: token not in url
+    fetch(`http://localhost:9000/user/authenticate?${searchParams.toString()}`, {
+      method: "GET",
+      headers: {"Content-Type": "application/x-www-form-urlencoded"}
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      else {
+        throw new Error("Not logged in!");
+      }
+    })
+    .then(data => {
+      console.log(data);
+      this.setState({loggedIn: true});
+    })
+    .catch(error => {
+      console.log(error);
+      this.setState({loggedIn: false});
+    });
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />
+    }    
   return (
     <div className="App">
-      <RegistrationForm />
+        {/* Render Posts component only after loggedIn state has been checked */}
+        {this.state.loggedIn !== null && (
+          <Route 
+            exactly 
+            render={(props) => <Posts {...props} loggedIn={this.state.loggedIn} />} 
+            path="/posts" 
+          />
+        )}
+        <Route 
+          exactly 
+          render={(props) => <Login {...props} handler={this.handler} />} 
+          path="/login" 
+        />
+        <Route exactly component={RegistrationForm} path="/register" />
     </div>
   );
 }
-
-export default App;
+}
