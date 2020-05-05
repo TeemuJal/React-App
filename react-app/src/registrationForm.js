9eb6df611bcd3dcaf0d2afbedebd9204c2a27b6f
Teemu Jalonen <teemu.jalonen@student.tut.fi>
2020-05-05 17:18:26 +0300

    Refactored RegistrationForm to be a class

--- react-app/src/registrationForm.js
+++ react-app/src/registrationForm.js
@@ -1,87 +1,115 @@
 import React from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import { Link, Redirect } from 'react-router-dom';
 
-const RegistrationForm = () => (
-  <div>
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={(values, { setSubmitting }) => {
-        fetch("http://localhost:9000/register", {
-          method: "POST",
-          headers: {"Content-Type": "application/x-www-form-urlencoded"},
-          body: new URLSearchParams({
-            "email": values.email,
-            "password": values.password
-          })
-        })
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          console.log(data);
-        });
-        setSubmitting(false);
-      }}
+export default class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRegistered: false,
+      error: null
+    };
+  }
 
-      validationSchema={Yup.object().shape({
-        email: Yup.string()
-          .email()
-          .required("Required"),
-        password: Yup.string()
-          .required("No password provided.")
-          .min(8, "Password is too short - should be 8 chars minimum.")
-          .matches(/(?=.*[0-9])/, "Password must contain a number.")
-      })}
-  >
-      { props => {
-        const {
-          values,
-          touched,
-          errors,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit
-        } = props;
-        return (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input
-              name="email"
-              type="text"
-              placeholder="Enter your email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.email && touched.email && "error"}
-            />
-            {errors.email && touched.email && (
-              <div className="input-feedback">{errors.email}</div>
-            )}
+  render() {
+    const error = this.state.error;
+    if (this.state.isRegistered) {
+      return <Redirect to="/login" />;
+    } 
+    return (
+      <div>
+        {error && <p>{error.message}</p>}
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            fetch("http://localhost:9000/register", {
+              method: "POST",
+              headers: {"Content-Type": "application/x-www-form-urlencoded"},
+              body: new URLSearchParams({
+                "email": values.email,
+                "password": values.password
+              })
+            })
+            .then(response => {
+              if (response.ok) {
+                return response.json();
+              }
+              else {
+                throw new Error("Invalid email/password");
+              }
+            })
+            .then(data => {
+              console.log(data);
+              this.setState({ isRegistered: true });
+            })
+            .catch(error => {
+              this.setState({ error })
+            });
+            setSubmitting(false);
+          }}
 
-            <label htmlFor="password">Password</label>
-            <input
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.password && touched.password && "error"}
-            />
-            {errors.password && touched.password && (
-              <div className="input-feedback">{errors.password}</div>
-            )}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email()
+              .required("Required"),
+            password: Yup.string()
+              .required("No password provided.")
+              .min(8, "Password is too short - should be 8 chars minimum.")
+              .matches(/(?=.*[0-9])/, "Password must contain a number.")
+          })}
+        >
+          { props => {
+            const {
+              values,
+              touched,
+              errors,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit
+            } = props;
+            return (
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input
+                  name="email"
+                  type="text"
+                  placeholder="Enter your email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.email && touched.email && "error"}
+                />
+                {errors.email && touched.email && (
+                  <div className="input-feedback">{errors.email}</div>
+                )}
 
-            <button type="submit" disabled={isSubmitting}>
-              Register
-            </button>
-          </form>
-        );
-      }}
-    </Formik>
-  </div>
-);
+                <label htmlFor="password">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.password && touched.password && "error"}
+                />
+                {errors.password && touched.password && (
+                  <div className="input-feedback">{errors.password}</div>
+                )}
 
-export default RegistrationForm;
+                <button type="submit" disabled={isSubmitting}>
+                  Register
+                </button>
+              </form>
+            );
+          }}
+        </Formik>
+        <div>
+          <Link className="Nav__link" to="/login">Already got an account? Login here!</Link>
+        </div>
+      </div>
+    )
+  }
+}
