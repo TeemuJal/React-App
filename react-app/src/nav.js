adfd5ee1c493e9dd1a7edbe4f37c4ae1349d8eb1
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-05-11 11:31:45 +0300

    Added navigation bar to the frontend

---
+++ react-app/src/nav.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
+export default class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: props.loggedIn
+    };
+  }
+  logoutHandler = (e) => {
+    localStorage.removeItem("userToken");
+    this.props.handler(false);
+    this.props.history.push('/login');
+  }
+  loginHandler = (e) => {
+    this.props.history.push('/login');
+  }
+  render() {
+    return (
+      <nav className="Nav">
+        <div className="Nav__container">
+          <div className="Nav__right">
+            <ul className="Nav__item-wrapper">
+              <li className="Nav__item">
+                <Link className="Nav__link" to="/posts">Posts</Link>
+              </li>
+              {this.state.loggedIn && (
+                <li className="Nav__item">
+                  <button onClick={e=>this.logoutHandler(e)}>Logout</button>
+                </li>
+              )}
+              {!this.state.loggedIn && (
+                <li className="Nav__item">
+                  <button onClick={e=>this.loginHandler(e)}>Login</button>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
